67707b0c4669cb4d8a9f7b80d8a6434720e10bbc
Dhruv S Patel <71836462+DHRUV6029@users.noreply.github.com>
2022-08-12 17:28:58 -0700

    Time: 1 ms (37.92%), Space: 44.2 MB (78.18%) - LeetHub

---
+++ 328-odd-even-linked-list/328-odd-even-linked-list.java
@@ -0,0 +1,32 @@
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        ListNode dummy_odd = new ListNode(-1);
+        ListNode dummy_even = new ListNode(-1);
+        
+        ListNode odd = dummy_odd;
+        ListNode even = dummy_even;
+        
+        int n = 0;
+        ListNode curr = head;
+        
+        while(curr != null){
+            n++;
+            
+            if(n % 2 == 1){
+                odd.next = curr;
+                odd = odd.next;
+            }
+            else{
+                even.next = curr;
+                even = even.next;
+            }
+            
+            curr = curr.next;
+        }
+        
+        even.next = null;
+        odd.next = dummy_even.next;
+        
+        return dummy_odd.next;
+    }
+}
